9108eb7d9a1acfb386b7a01f5fd35ec369efc71d
wajihajafri <jafriwajiha14@gmail.com>
2025-01-30 09:05:38 +0000

    datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,21 @@
+console.log(3+3)
+console.log("wajiha")
+// code readability should be high
+
+console.log("jafri")
+
+let name = "wajiha"
+let age ="18"
+let isLoggedIn = false
+let state;
+
+// num=> 2 to the power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined=>
+//symbols=> unique
+
+console.log(typeof null) //object 
+console.log(undefined) //undefined
