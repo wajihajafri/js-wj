7755a808859cf9733f222b867b62ab8e74bff791
wajihajafri <jafriwajiha14@gmail.com>
2025-01-30 10:56:34 +0000

    conversionOperater

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let str1 = "hello"
+let str2 =" world"
+
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2 + 3)
+//console.log(1 + 2 + "3")
+
+//console.log(+true)
+//console.log(+"")
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
